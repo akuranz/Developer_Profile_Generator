1bb17f3492c2bffaa2bc49813b42a2026d666a7f
Abby Kuranz <akuranz@gmail.com>
2019-12-31 13:56:41 -0700

    pulled information from GitHub API for single username

--- index.js
+++ index.js
@@ -18,21 +18,30 @@ inquirer
     }
   ])
   .then(function({ username, colors }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    const queryUrl = `https://api.github.com/users/${username}`;
     axios.get(queryUrl).then(function(res) {
-      const repoNames = res.data.map(function(repo) {
-        return repo.name;
-      });
+      console.log(res);
+      console.log(res.data.location);
+      console.log(res.data.html_url);
+      console.log(res.data.blog);
+      console.log(res.data.bio);
+      console.log(res.data.public_repos);
+      console.log(res.data.followers);
+      console.log(res.data.following);
+      console.log(res.data.public_gists);
+      //   const repoNames = res.data.map(function(repo) {
+      //     return repo.name;
+      //   });
 
       //   const repoNamesStr = repoNames.join("\n");
       const repoNamesStr = `<p style="${colors};">Hello</p>`;
 
-      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+      fs.writeFile("resume.html", repoNamesStr, function(err) {
         if (err) {
           throw err;
         }
 
-        console.log(`Saved ${repoNames.length} repos`);
+        // console.log(`Saved ${repoNames.length} repos`);
       });
     });
   });
